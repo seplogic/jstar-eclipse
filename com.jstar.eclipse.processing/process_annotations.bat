10063deee3aadaf15fa1166912f0afd3b464e1bd
Daiva Naudziuniene <da319@cam.ac.uk>
2010-10-25 16:57:44 +0100

    Missing lib files.

--- com.jstar.eclipse.processing/process_annotations.bat
+++ com.jstar.eclipse.processing/process_annotations.bat
@@ -1,8 +1,9 @@
 @echo off
 set JAR_PROCESSING=C:\Users\da319\workspace\jstar\com.jstar.eclipse.processing\jar file\jstar_processing.jar
 set JAR_LIB=C:\Users\da319\workspace\jstar\com.jstar.eclipse.processing\lib\commons-io-1.4\commons-io-1.4.jar
+set JAR_LIB_LANG=C:\Users\da319\workspace\jstar\com.jstar.eclipse.processing\lib\commons-lang-2.5\commons-lang-2.5.jar
 set JAR_ANNOT=C:\Users\da319\workspace\jstar\com.jstar.eclipse.annotations\jar file\annotations.jar
-set JARS="%CLASSPATH%;.;%JAR_PROCESSING%;%JAR_LIB%;%JAR_ANNOT%"
+set JARS="%CLASSPATH%;.;%JAR_PROCESSING%;%JAR_LIB%;%JAR_LIB_LANG%;%JAR_ANNOT%"
 set PROC=com.jstar.eclipse.processing.SpecAnnotationProcessor
 echo on
 
